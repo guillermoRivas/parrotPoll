6362f062e08e13e4dfe8ab078288001fdf4319fc
guillermoRivas <guille.tj4@gmail.com>
2016-07-22 10:03:48 +0200

    RF01

--- public/app/login/login.js
+++ public/app/login/login.js
@@ -1,4 +1,16 @@
 angular.module('parrotPollApp')
-    .controller('loginCtrl', function($scope,$http) {
-      
+    .controller('loginCtrl', function($scope, $http) {
+
+        $scope.submit = function() {
+            var post = $http.post('api/user', $scope.userReg).then(
+                function(res) {
+                    // success callback
+                    $scope.usuario = res.data;
+                    $scope.userReg = undefined;
+                },
+                function(response) {
+                    // failure callback
+                }
+            );
+        };
     });
